3c0df8828d284afdcb47a2c6c12818f9d4e3a90c
Aidaraly <aidaraly.anarbaev@iaau.edu.kg>
2020-12-11 21:30:20 +0600

    Inital commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,75 @@
+package sample;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+import org.json.JSONObject;
+
+public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    // Переменные, ссылающиеся на объекты из окна приложения
+    @FXML
+    private TextField city;
+
+    @FXML
+    private Text temp_info;
+
+    @FXML
+    private Text temp_max;
+
+    @FXML
+    private Text temp_min;
+
+    @FXML
+    private Button getData;
+
+    @FXML
+    private Text temp_feels;
+
+    @FXML
+    private ImageView image;
+
+    @FXML
+    private Text pressure;
+
+    @FXML
+    void initialize() {
+        // При нажатии на кнопку
+
+    }
+
+    // Обработка URL адреса и получение данных с него
+    private static String getUrlContent(String urlAdress) {
+        StringBuffer content = new StringBuffer();
+
+        try {
+            URL url = new URL(urlAdress);
+            URLConnection urlConn = url.openConnection();
+
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConn.getInputStream()));
+            String line;
+
+            while((line = bufferedReader.readLine()) != null) {
+                content.append(line + "\n");
+            }
+            bufferedReader.close();
+        } catch(Exception e) {
+            System.out.println("Такой город был не найден!");
+        }
+        return content.toString();
+    }
+
+}
