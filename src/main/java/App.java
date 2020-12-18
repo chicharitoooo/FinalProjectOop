80d54880984fed91f1db31e7056bef173ff7599e
Anvar999 <Anvarbek.anvarov@iaau.edu.kg>
2020-12-19 02:26:23 +0600

    Last Commit

---
+++ src/main/java/App.java
@@ -0,0 +1,32 @@
+import database.WorldCreatorDatabase;
+import helpers.Loader;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    private WorldCreatorDatabase database;
+    Loader loader = new Loader();
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        createDatabase();
+        primaryStage.setTitle("World Creator");
+        primaryStage.setScene(loader.getScene("HelloScene"));
+        primaryStage.setMaximized(false);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private void createDatabase() {
+        this.database = new WorldCreatorDatabase();
+    }
+
+    public WorldCreatorDatabase getDatabase() {
+        return database;
+    }
+
+}
